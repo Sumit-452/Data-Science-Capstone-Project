9a95657281d668297945bdc2a8f6a234ceef0aa2
Sumit-452 <122899296+Sumit-452@users.noreply.github.com>
2023-01-17 22:08:15 +0530

    Update car_p.py

--- car_p.py
+++ car_p.py
@@ -1,7 +1,5 @@
 import pandas as pd
-
 import numpy as np
-
 from sklearn.preprocessing import *
 
 df = pd.read_csv('CAR DETAILS.csv')
@@ -9,11 +7,21 @@ df = pd.read_csv('CAR DETAILS.csv')
 lst=[i.split(' ')[0] for i in df['name']]
 df['manufacturer']=lst
 
+df=pd.get_dummies(data=df,columns=['fuel','seller_type','owner','transmission','manufacturer'])
+
+x=df.drop(columns=['selling_price'],axis=1)
+y=df['selling_price']
+
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+xmm = MinMaxScaler().fit_transform(x)
+
+
+
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
 
 bg=AdaBoostRegressor(base_estimator=DecisionTreeRegressor())
-bg.fit(x_train,y_train)
+bg.fit(xmm,y)
 
 import pickle
 pickle.dump(ad,open('best_model.pkl','wb'))
