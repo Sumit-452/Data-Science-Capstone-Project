77ac93d4cbd3f166474246e1ffee9cf78b1c4128
Sumit-452 <122899296+Sumit-452@users.noreply.github.com>
2023-01-17 23:26:23 +0530

    Update car_app.py

--- car_app.py
+++ car_app.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 Trans = st.radio("Select Transmission: ", ('Automatic', 'Manual'))
-#model = pickle.load(open('best_model.pkl','rb'))
+
 
 owner = st.selectbox("owner: ",
                      ['First Owner','Second Owner','Third Owner','Fourth & Above Owner','Test Drive Car'])
