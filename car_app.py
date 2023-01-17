f8ad955bc4e1a25c21077c1edbf936cd5aab5ee0
Sumit-452 <122899296+Sumit-452@users.noreply.github.com>
2023-01-17 23:38:41 +0530

    Update car_app.py

--- car_app.py
+++ car_app.py
@@ -2,10 +2,20 @@ import pickle
 
 import streamlit as st
 
-Trans = st.radio("Select Transmission: ", ('Automatic', 'Manual'))
+
+st.title('car prediction model')
+transmission = st.radio("Select Transmission: ", ('Automatic', 'Manual'))
 
 
 owner = st.selectbox("owner: ",
                      ['First Owner','Second Owner','Third Owner','Fourth & Above Owner','Test Drive Car'])
                          
-st.title('car prediction model')
+manufacturer = st.selectbox("manufacturer: ",
+                     ['Maruti','Hyundai','Mahindra','Tata','Ford','Honda','Toyota','Chevrolet','Renault',
+                      'Volkswagen','Nissan','Skoda','Fiat','Audi','Datsun','BMW','Mercedes-Benz'])
+
+seller_type = st.radio("select seller type: ",
+                     ('Individual','Dealer','Trustmark Dealer'))
+
+fuel = st.selectbox("fuel: ",
+                     ['Diesel','Petrol','CNG','LPG','Electric'])
