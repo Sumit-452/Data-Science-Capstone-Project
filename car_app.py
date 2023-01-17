ae44422aff17735a7ff1b84addef24478a0e51ac
Sumit-452 <122899296+Sumit-452@users.noreply.github.com>
2023-01-17 23:24:02 +0530

    Update car_app.py

--- car_app.py
+++ car_app.py
@@ -2,8 +2,12 @@ import pickle
 
 import streamlit as st
 
-model = pickle.load(open('best_model.pkl','rb'))
+year=
 
+Trans = st.radio("Select Transmission: ", ('Automatic', 'Manual'))
+#model = pickle.load(open('best_model.pkl','rb'))
 
+owner = st.selectbox("owner: ",
+                     ['First Owner','Second Owner','Third Owner','Fourth & Above Owner','Test Drive Car'])
                          
 st.title('car prediction model')
